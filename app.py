734ce3371a79e7207e57256955ef67c79b957bb9
Ilyas <ibb30@alu.ua.es>
2023-06-18 12:19:56 +0200

    Implementacion de CORS

--- app.py
+++ app.py
@@ -5,6 +5,16 @@ import uvicorn
 
 app = FastAPI(title="TheBestLiga API")
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/listaJugadores")
 def getListaJugadores():
